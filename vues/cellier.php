7fe363f5fe173e83767a99ec92c44fdd174af050
Pauline <huby.pauline@gmail.com>
2023-01-17 17:15:42 -0500

    Ajout affichage conditionnel millesime page cellier

--- vues/cellier.php
+++ vues/cellier.php
@@ -24,7 +24,9 @@
                 <p class="type"><?php echo $bouteille['type'] ?></p>          
                 <p class="nom"><?php echo $bouteille['nom'] ?></p>
                 <p class="pays"><?php echo $bouteille['pays'] ?></p>
-                <p class="millesime">Millesime <?php echo $bouteille['millesime'] ?></p>
+                <?php if ($bouteille['millesime'] != 0) : ?>
+                    <p class="millesime">Millesime <?php echo $bouteille['millesime'] ?></p>
+                <?php endif; ?>
                 <div class="quantity-wrapper">
                     <div class="bouton-carre btnBoire" data-id="<?php echo $bouteille['id_bouteille_cellier'] ?>">
                         <i class="fa-solid fa-minus btnBoire" data-id="<?php echo $bouteille['id_bouteille_cellier'] ?>"></i>
