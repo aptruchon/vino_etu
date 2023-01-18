0fe5f769117c8f247c3e3d6bf0eb97cf6991f188
Pauline <huby.pauline@gmail.com>
2023-01-18 15:53:02 -0500

    Ajout champs et boutons type vin page ajout

--- vues/ajouter.php
+++ vues/ajouter.php
@@ -11,11 +11,28 @@
             <label for="nom">Nom</label>
             <input type="text" class="nom" name="nom" data-id="" id="nom">
             <span class="champ-obligatoire-message"></span>
+            <label for="pays">Pays</label>
+            <input type="text" class="pays" name="pays" data-id="" id="pays">
+            <span class="champ-obligatoire-message"></span>
+            <label for="format">Format</label>
+            <input type="text" class="format" name="format" data-id="" id="format">
+            <span class="champ-obligatoire-message"></span>
             <label for="millesime">Millesime</label>
             <input type="text" name="millesime" id="millesime">
             <span class="champ-obligatoire-message"></span>
+            <label for="description">Description</label>
+            <textarea name="description" id="description"></textarea>
+            <span class="champ-obligatoire-message"></span>
             <label for="quantite">Quantite</label>
             <input type="number" name="quantite" id="quantite" value="1">
+            <span class="champ-obligatoire-message"></span>
+            <label>Type</label>
+            <div class="options-container">
+                <input type="radio" id="rouge" name="type" value="rouge" class="radio-input">
+                <label for="rouge" class="radio-label">Rouge</label><br>
+                <input type="radio" id="blanc" name="type" value="blanc" class="radio-input">
+                <label for="blanc" class="radio-label">Blanc</label><br>
+            </div>
             <span class="champ-obligatoire-message"></span>
             <label for="date_achat">Date achat</label>
             <input type="text" name="date_achat" id="date_achat">
