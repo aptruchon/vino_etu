497a037d128d9f75a8cb342ad55b27a826a9de8e
alanamoraes <alanaaaa@hotmail.com>
2023-01-19 15:01:19 -0500

    modification pour rendre le lien modifier au click de la carte

--- vues/cellier.php
+++ vues/cellier.php
@@ -14,13 +14,16 @@
         ?>
         <div class="bouteille" data-quantite="<?php echo $bouteille['quantite'] ?>">
             <div class="bouteille-img-container">
+                <a href="?requete=modifierBouteilleCellier">
                 <?php if ($bouteille['image']) : ?>
-                    <img src="https:<?php echo $bouteille['image'] ?>">
+                   <img src="https:<?php echo $bouteille['image'] ?>"> 
                 <?php else: ?>
                     <img src="./images/vin-fallback.png">
                 <?php endif; ?>
+                </a>
             </div>
             <div class="description">
+                <a href="?requete=modifierBouteilleCellier">
                 <p class="type <?php echo strtolower($bouteille['type']) ?>"><?php echo $bouteille['type'] ?></p>          
                 <p class="nom"><?php echo $bouteille['nom'] ?></p>
                 <p class="pays"><?php echo $bouteille['pays'] ?></p>
@@ -36,7 +39,8 @@
                         <i class="fa-solid fa-plus btnAjouter" data-id="<?php echo $bouteille['id_bouteille_cellier'] ?>"></i>
                     </div>
                 </div>
-                <a href="?requete=modifierBouteilleCellier">Modifier</a>
+                <!-- <a href="?requete=modifierBouteilleCellier">Modifier</a> -->
+                </a>
             </div>
         </div>
     <?php
