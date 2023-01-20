15af74ea09ff04e2e54859f4c762aa74fdd8d5bc
alanamoraes <alanaaaa@hotmail.com>
2023-01-20 15:59:58 -0500

    supprimer les codes inutiles

--- vues/accueil.php
+++ vues/accueil.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/home-page.css">
-    <title>Page d'accueil</title>
-</head>
-<body>
     <div class="container">
     <section class="home-page">
             <div class="coin-superieur">
@@ -38,5 +28,3 @@
             </div>
     </section>
     </div>
-</body>
-</html>
