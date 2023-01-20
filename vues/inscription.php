9cfc68c31594bcef188308bf3ce6ed946631858a
Pauline <huby.pauline@gmail.com>
2023-01-20 15:05:08 -0500

    Ajout style page inscription

--- vues/inscription.php
+++ vues/inscription.php
@@ -1,5 +1,7 @@
-<section class="">
-    <h2>Bienvenue !</h2>
+<section class="login-container">
+    <header class="page-content-header"> 
+        <h2>Bienvenue !</h2>
+    </header>  
     <div class="infos-wrapper">
         <label for="uti_prenom">Prénom</label>
         <input type="text" name="uti_prenom" id="uti_prenom">
@@ -12,13 +14,13 @@
         <label for="uti_mdp">Mot de passe</label>
         <input type="text" name="uti_mdp" id="uti_mdp">
     </div>
-    <div>
+    <div class="mdp-infos">
         <p>Doit inclure :</p>
         <span>8 caractères minimum</span>
         <span>1 minuscule</span>
         <span>1 majuscule</span>
         <span>1 chiffre</span>
     </div>
-    <button>S'inscrire</button>
-    <div>Deja inscrit.e ? <a href="">Connectez-vous !</a></div>
+    <button class="bouton-large">S'inscrire</button>
+    <div class="login-link">Déjà inscrit.e ? <a href="?requete=connexion" class="link-bold">Connectez-vous !</a></div>
 </section>
