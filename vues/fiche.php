594341f61736c2411960873bf226c64bc96168ef
Claudia Lisboa <ccalumby@gmail.com>
2023-01-23 10:30:15 -0500

    FEAT : Ajouter nouvelle fonctionnalité  au cellier

--- vues/fiche.php
+++ vues/fiche.php
@@ -1,23 +1,35 @@
 <div class="fiche">
     <a href="?requete=cellier" class="retour">
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="#931818" d="M228 128a12 12 0 0 1-12 12H69l51.5 51.5a12 12 0 0 1 0 17a12.1 12.1 0 0 1-17 0l-72-72a12 12 0 0 1 0-17l72-72a12 12 0 0 1 17 17L69 116h147a12 12 0 0 1 12 12Z"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256">
+            <path fill="#931818" d="M228 128a12 12 0 0 1-12 12H69l51.5 51.5a12 12 0 0 1 0 17a12.1 12.1 0 0 1-17 0l-72-72a12 12 0 0 1 0-17l72-72a12 12 0 0 1 17 17L69 116h147a12 12 0 0 1 12 12Z" />
+        </svg>
     </a>
-    <section class="bouteille_container">  
-        <img src="" alt="">
-        <div class="description">
-            <p class="type">Type</p>          
-            <p class="nom">Nom</p>
-            <p class="pays">Pays</p>
-            <p class="format">Format</p>
-            <p class="millesime">Millesime</p>
-            <p class="description">Description</p>
-            <p class="date_ajout">Date ajout</p>
-            <p class="garde_jusqua">garde jusqu'a</p>
-            <p class="notes">Notes</p>
-            <p class="prix">Prix payé</p>
-            <p class="prix_saq">Prix SAQ</p>
-        </div>
-    </section> 
-    <a href="?requete=modifierBouteilleCellier">Modifier</a>
+    <?php
+    foreach ($data as $cle => $bouteille) {
+    ?>
+        <section class="bouteille_container">
+            <?php if ($bouteille['image']) : ?>
+                <img src="https:<?php echo $bouteille['image'] ?>">
+            <?php else : ?>
+                <img src="./images/vin-fallback.png">
+            <?php endif; ?>
+            <div class="description">
+                <p class="type">Type : <?php echo $bouteille['type'] ?></p>
+                <p class="nom"><?php echo $bouteille['nom'] ?></p>
+                <p class="pays">Pays : <?php echo $bouteille['pays'] ?></p>
+                <p class="format">Format : <?php echo $bouteille['format'] ?></p>
+                <p class="millesime">Millesime : <?php echo $bouteille['millesime'] ?></p>
+                <p class="description">Description : <?php echo $bouteille['description'] ?></p>
+                <p class="date_ajout">Date ajout : <?php echo $bouteille['date_ajout'] ?></p>
+                <p class="garde_jusqua">garde jusqu'a : <?php echo $bouteille['garde_jusqua'] ?></p>
+                <p class="notes">Notes : <?php echo $bouteille['notes'] ?></p>
+                <p class="prix">Prix payé : <?php echo '$ ' . $bouteille['prix_paye'] ?></p>
+                <p class="prix_saq">Prix SAQ : <?php echo '$ ' . $bouteille['prix_saq'] ?></p>
+            </div>
+        </section>
+        <a href="?requete=modifierBouteilleCellier&bte=<?php echo $bouteille['vino__bouteille_id'] ?>">Modifier</a>
+    <?php
+    }
+    ?>
 
 </div>
