a94c6df99681b825b3905c6434fe756a984293c0
alanamoraes <alanaaaa@hotmail.com>
2023-01-25 13:19:01 -0500

    ajout visibility hidden au svg pour cacher pour la presentation vendredi

--- vues/navigation.php
+++ vues/navigation.php
@@ -23,7 +23,7 @@
             <?php else : ?>
                 <h2 class="titre" name="titre"></h2>
             <?php endif ; ?>
-            <svg class="icones-sign-in" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256">
+            <svg class="icones-sign-in" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256" visibility="hidden">
                 <path fill="#ffffff" d="m144.5 136.5l-42 42A12 12 0 0 1 94 182a12.2 12.2 0 0 1-8.5-3.5a12 12 0 0 1 0-17L107 140H24a12 12 0 0 1 0-24h83L85.5 94.5a12 12 0 0 1 17-17l42 42a12 12 0 0 1 0 17ZM192 28h-56a12 12 0 0 0 0 24h52v152h-52a12 12 0 0 0 0 24h56a20.1 20.1 0 0 0 20-20V48a20.1 20.1 0 0 0-20-20Z"/>
             </svg>
         <!-- </div> -->
