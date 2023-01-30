cb910c24ddf5541454a7e5a398e10aa1f773969d
alanamoraes <alanaaaa@hotmail.com>
2023-01-30 12:08:47 -0500

    add icon X pour la fermeture de la boite modale

--- js/main.js
+++ js/main.js
@@ -182,7 +182,7 @@ window.addEventListener('load', function() {
         fetch(requete)
           .then((response) => {
             if (response.status === 200) {
-              return response.json()
+              return response.json
             } else {
               throw new Error('Erreur')
             }
